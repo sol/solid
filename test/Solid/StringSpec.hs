1fe77cc2bc6b4e70412434cac736e48335f1a990
Simon Hengel <sol@typeful.net>
2022-09-24 19:16:46 +0700

    Add test for `unpack`

--- test/Solid/StringSpec.hs
+++ test/Solid/StringSpec.hs
@@ -10,6 +10,11 @@ import qualified Data.Text as T
 
 spec :: Spec
 spec = do
+  describe "unpack" $ do
+    it "is inverse to pack" $ do
+      property $ \ xs -> do
+        unpack (pack xs) `shouldBe` xs
+
   describe "String.length" $ do
     it "returns the length of a String" $ do
       property $ \ xs -> do
