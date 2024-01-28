f2a619c0f02e44b8e830712d00c5b981bab4a926
Simon Hengel <sol@typeful.net>
2024-01-28 12:29:21 +0700

    Move unsafe primitives to `Data.Sliced.ByteArray.Unsafe`

--- src/Data/Sliced/ByteArray.hs
+++ src/Data/Sliced/ByteArray.hs
@@ -583,46 +583,6 @@ memchr (Array.ByteArray bytes) off len = c_memchr bytes (fromIntegral off) (from
 foreign import ccall unsafe "_hs_text_memchr" c_memchr
   :: ByteArray# -> CSize -> CSize -> Word8 -> CSsize
 
-unsafeIndex :: Int -> ByteArray -> Word8
-unsafeIndex n bytes = Array.unsafeIndex bytes.arr (bytes.off + n)
-{-# INLINE unsafeIndex #-}
-
-unsafeHead :: ByteArray -> Word8
-unsafeHead bytes = Array.unsafeIndex bytes.arr bytes.off
-{-# INLINE unsafeHead #-}
-
-unsafeLast :: ByteArray -> Word8
-unsafeLast bytes = Array.unsafeIndex bytes.arr (bytes.off + bytes.len - 1)
-{-# INLINE unsafeLast #-}
-
-unsafeTail :: ByteArray -> ByteArray
-unsafeTail = unsafeDrop 1
-{-# INLINE unsafeTail #-}
-
-unsafeInit :: ByteArray -> ByteArray
-unsafeInit = unsafeDropEnd 1
-{-# INLINE unsafeInit #-}
-
-unsafeTake :: Int -> ByteArray -> ByteArray
-unsafeTake n (ByteArray arr off _) = ByteArray arr off n
-{-# INLINE unsafeTake #-}
-
-unsafeTakeEnd :: Int -> ByteArray -> ByteArray
-unsafeTakeEnd n (ByteArray arr off len) = ByteArray arr (off + len - n) n
-{-# INLINE unsafeTakeEnd #-}
-
-unsafeDrop :: Int -> ByteArray -> ByteArray
-unsafeDrop n (ByteArray arr off len) = ByteArray arr (off + n) (len - n)
-{-# INLINE unsafeDrop #-}
-
-unsafeDropEnd  :: Int -> ByteArray -> ByteArray
-unsafeDropEnd n (ByteArray arr off len) = ByteArray arr off (len - n)
-{-# INLINE unsafeDropEnd #-}
-
-unsafeSlice :: Int -> Int -> ByteArray -> ByteArray
-unsafeSlice start end (ByteArray arr off _) = ByteArray arr (off + start) (end - start)
-{-# INLINE unsafeSlice #-}
-
 nothingOnEmpty :: (ByteArray -> a) -> ByteArray -> Maybe a
 nothingOnEmpty f = withNonEmpty Nothing (Just . f)
 {-# INLINE nothingOnEmpty #-}
