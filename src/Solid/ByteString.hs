59c3d4813eff3f894c266669172468f68714f598
Simon Hengel <sol@typeful.net>
2024-01-28 12:09:28 +0700

    Add `ByteString.empty?`

--- src/Solid/ByteString.hs
+++ src/Solid/ByteString.hs
@@ -4,8 +4,10 @@ module Solid.ByteString where
 
 import Solid.Common
 import Solid.Bytes.Unsafe
+import GHC.Exts
 
 type ByteString = Bytes Word8
 
 deriving newtype instance Semigroup ByteString
 deriving newtype instance Monoid ByteString
+deriving newtype instance IsList ByteString
