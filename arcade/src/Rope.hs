99c6cdc20b0523294a1425d52bce9b8dd723ea0e
Simon Hengel <sol@typeful.net>
2024-05-08 20:59:15 +0700

    arcade: Use method syntax

--- arcade/src/Rope.hs
+++ arcade/src/Rope.hs
@@ -33,81 +33,48 @@ fromString :: String -> Rope
 fromString = TextRope.fromText . Haskell.toText
 
 -- | O(n)
-toString :: Rope -> String
-toString = Haskell.fromText . TextRope.toText
+.toString :: Rope -> String
+.toString = Haskell.fromText . TextRope.toText
 
 -- | O(1)
-null :: Rope -> Bool
-null = TextRope.null
+.null :: Rope -> Bool
+.null = TextRope.null
 
 -- Each line is produced in O(1).
-lines :: Rope -> [String]
-lines = map Haskell.fromText . TextRope.lines
+.lines :: Rope -> [String]
+.lines = map Haskell.fromText . TextRope.lines
 
 -- | O(log n)
-lengthInLines :: Rope -> Int
-lengthInLines = toInt . TextRope.lengthInLines
+.lengthInLines :: Rope -> Int
+.lengthInLines = toInt . TextRope.lengthInLines
 
 -- | O(log n)
-takeLines :: Int -> Rope -> Rope
-takeLines n = fst . splitAtLine n
+.takeLines :: Int -> Rope -> Rope
+.takeLines n = fst . splitAtLine n
 
 -- | O(log n)
-dropLines :: Int -> Rope -> Rope
-dropLines n = snd . splitAtLine n
+.dropLines :: Int -> Rope -> Rope
+.dropLines n = snd . splitAtLine n
 
 -- | O(log n)
-splitAtLine :: Int -> Rope -> (Rope, Rope)
-splitAtLine = TextRope.splitAtLine . fromInt
+.splitAtLine :: Int -> Rope -> (Rope, Rope)
+.splitAtLine = TextRope.splitAtLine . fromInt
 
 -- | O(1)
-length :: Rope -> Int
-length = toInt . TextRope.length
+.length :: Rope -> Int
+.length = toInt . TextRope.length
 
 -- | O(n)
-splitAt :: WithStackTrace => Int -> Rope -> (Rope, Rope)
-splitAt = TextRope.splitAt . fromInt
+.splitAt :: WithStackTrace => Int -> Rope -> (Rope, Rope)
+.splitAt = TextRope.splitAt . fromInt
 
 -- | O(1)
-lengthAsPosition :: Rope -> Position
-lengthAsPosition = TextRope.lengthAsPosition
+.lengthAsPosition :: Rope -> Position
+.lengthAsPosition = TextRope.lengthAsPosition
 
 -- Time is linear in 'posColumn' and logarithmic in 'posLine'.
-splitAtPosition :: WithStackTrace => Position -> Rope -> (Rope, Rope)
-splitAtPosition = TextRope.splitAtPosition
-
-instance HasField "toString" Rope String where
-  getField = Rope.toString
-
-instance HasField "null" Rope Bool where
-  getField = TextRope.null
-
-instance HasField "lines" Rope [String] where
-  getField = lines
-
-instance HasField "lengthInLines" Rope Int where
-  getField = lengthInLines
-
-instance HasField "takeLines" Rope (Int -> Rope) where
-  getField = flip takeLines
-
-instance HasField "dropLines" Rope (Int -> Rope) where
-  getField = flip dropLines
-
-instance HasField "splitAtLine" Rope (Int -> (Rope, Rope)) where
-  getField = flip splitAtLine
-
-instance HasField "length" Rope Int where
-  getField = length
-
-instance HasField "splitAt" Rope (Int -> (Rope, Rope)) where
-  getField = flip splitAt
-
-instance HasField "lengthAsPosition" Rope Position where
-  getField = lengthAsPosition
-
-instance HasField "splitAtPosition" Rope (Position -> (Rope, Rope)) where
-  getField = flip splitAtPosition
+.splitAtPosition :: WithStackTrace => Position -> Rope -> (Rope, Rope)
+.splitAtPosition = TextRope.splitAtPosition
 
 toInt :: Word -> Int
 toInt = fromIntegral
